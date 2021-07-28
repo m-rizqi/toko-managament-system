76b357729b6206c1982368b4755a8e7c2382e1cf
Muhammad Rizqi <mrizqi070502@gmail.com>
2021-07-28 22:36:15 +0700

    list harga textview

---
+++ app/src/main/java/com/example/tokomanagementsystem/view/NewProductActivity.java
@@ -0,0 +1,16 @@
+package com.example.tokomanagementsystem.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tokomanagementsystem.R;
+
+public class NewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_product);
+    }
+}
